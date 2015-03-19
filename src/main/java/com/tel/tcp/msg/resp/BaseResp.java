6076112e89639f5a38c937ae23f75d5a56e2eed5
zhousong <songsong11_zhou@163.com>
2015-03-19 17:11:46 +0800

    resp -- desc

--- src/main/java/com/tel/tcp/msg/resp/BaseResp.java
+++ src/main/java/com/tel/tcp/msg/resp/BaseResp.java
@@ -6,6 +6,7 @@ public class BaseResp extends BaseMsg {
 	private static final long serialVersionUID = 738210650685204314L;
 
 	private int code;
+	private String desc;
 
 	public int getCode() {
 		return code;
@@ -15,11 +16,21 @@ public class BaseResp extends BaseMsg {
 		this.code = code;
 	}
 
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
 		builder.append("BaseResp [code=");
 		builder.append(code);
+		builder.append(", desc=");
+		builder.append(desc);
 		builder.append(", toString()=");
 		builder.append(super.toString());
 		builder.append("]");
